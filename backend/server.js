9c83096b12cd05c8bc576e975e7e4398567f0c80
ChristopherCaysido <christopher.caysido@gmail.com>
2025-03-04 19:02:14 +0800

    feat: added the route /subscribe
    - Route takes the email, location and type of alert the user wants notifications of

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,28 @@ app.use(express.json())
 
 // PostgreSQL Setup
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    user: process.env.DB_USER,
+    host: process.env.NODE_ENV == 'Development' ? 'localhost' : process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: String(process.env.DB_PASS),
+    port: process.env.DB_PORT,
+})
+
+app.post('/subscribe', async (req,res) => {
+    const {email,location,alert_condition} = req.body
+    try {
+        await pool.query(
+            `INSERT INTO users (email, location, alert_condition) VALUES ($1,$2,$3)`,
+            [email,location,alert_condition]
+        )
+        res.status(201).json({message: 'Subscription successful!'})
+    } catch(err){
+        console.error(err);
+        res.status(500).json({error: 'Database Error'})
+    }
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
 })
